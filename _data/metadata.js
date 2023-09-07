e6c8717145cb37d76df374468c8a00ae4f697ee8
Antonio Pastorino <antonio.pastorino@gmail.com>
2023-09-07 09:33:56 +0200

    feat(:tada:): first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Eleventy Barebone",
+	url: "https://github.com/andou/eleventy-barebone",
+	language: "en",
+	description: "Yet another 11ty barebone website",
+	author: {
+		name: "Antonio Pastorino",
+		email: "antonio.pastorino@gmail.com",
+		url: "https://github.com/andou"
+	}
+};
